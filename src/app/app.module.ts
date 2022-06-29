4baa17e2867fcc123c0c6df29a8459d37e2b500f
Winn Nie <winnnie.wong@hexainitiative.com>
2022-06-29 13:47:48 +0800

    [] hmscore/cordova-plugin-hms-availability [] hmscore/ionic-native-hms-availability

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,12 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HMSAvailability } from '@hmscore/ionic-native-hms-availability/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, HMSAvailability],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
